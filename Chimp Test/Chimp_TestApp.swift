5f3a2757077d2d07a28c80317e70124830b036c5
Alistiar White <awhite@bsd220.org>
2023-01-12 09:30:06 -0600

    Initial Commit

---
+++ Chimp Test/Chimp_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chimp_TestApp.swift
+//  Chimp Test
+//
+//  Created by Alistair White on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Chimp_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
