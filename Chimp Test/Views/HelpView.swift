d5af7096808b20c72dfefb6624f652fe118d0710
Alistiar White <awhite@bsd220.org>
2023-01-12 18:36:10 -0600

    Additional file commits, same changes as before

--- Chimp Test/Views/HelpView.swift
+++ Chimp Test/Views/HelpView.swift
@@ -13,7 +13,28 @@ struct HelpView: View {
     let colorMiddle : Color
     let colorFront : Color
     var body: some View {
-        Text("Help View")
+        ZStack {
+            colorMiddle.ignoresSafeArea()
+            VStack {
+                Text("Are You Smarter Than a Chimpanzee?")
+                    .font(.largeTitle).bold()
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 20)
+                Text("Click the squares in order according to their numbers.\n The test will get progressively harder")
+                    .font(.title2).bold()
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 30)
+                Text("This is a test of working memory, made famous by a study that found that chimpanzees consistently outperform humans on this task.\n\n In the study, the chimps consistently outperformed humans, and some chimps were able to remember 9 digits over 90% of the time.\n\n This test is a variant of that concept, that gets increasingly difficult every turn, starting at 4 digits, and adding one every turn. If you pass a level, the number increases. If you fail, you get a strike. Three strikes and the test is over.")
+                    .font(.title3)
+                    .foregroundColor(.black)
+                    .padding(.horizontal, 10)
+                Spacer()
+                Text("Swipe down to close")
+                    .foregroundColor(colorBack)
+            }
+        }
     }
 }
 
--- Chimp Test/Views/LoseView.swift
+++ Chimp Test/Views/LoseView.swift
@@ -12,31 +12,16 @@ struct LoseView: View {
     let colorBack : Color
     let colorMiddle : Color
     let colorFront : Color
+    let loseCount : Int
+    let buttonCount : Int
     var body: some View {
-        ZStack {
-            colorFront.ignoresSafeArea()
-            VStack {
-                Text("Are You Smarter Than a Chimpanzee?")
-                    .font(.largeTitle).bold()
-                    .foregroundColor(colorMiddle)
-                    .multilineTextAlignment(.center)
-                    .padding(.vertical, 20)
-                Text("Click the squares in order according to their numbers.\n The test will get progressively harder")
-                    .font(.title2).bold()
-                    .foregroundColor(colorMiddle)
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 30)
-                Text("This is a test of working memory, made famous by a study that found that chimpanzees consistently outperform humans on this task.\n\n In the study, the chimps consistently outperformed humans, and some chimps were able to remember 9 digits over 90% of the time.\n\n This test is a variant of that concept, that gets increasingly difficult every turn, starting at 4 digits, and adding one every turn. If you pass a level, the number increases. If you fail, you get a strike. Three strikes and the test is over.")
-                    .font(.title3)
-                    .foregroundColor(colorMiddle)
-                    .padding(.horizontal, 10)
-                Spacer()
-            }
+        VStack {
+            Text("Lose View")
         }
     }
 }
 struct LoseView_Previews: PreviewProvider {
     static var previews: some View {
-        LoseView(colorBack: Color("Steel Teal"), colorMiddle: Color("Tea Green"), colorFront: Color("Charcoal"))
+        LoseView(colorBack: Color("Steel Teal"), colorMiddle: Color("Tea Green"), colorFront: Color("Charcoal"), loseCount: 1, buttonCount: 7)
     }
 }
